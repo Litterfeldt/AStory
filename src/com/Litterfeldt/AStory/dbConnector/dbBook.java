d4ef7df8ee69acefc25d2017af3ea51c5ef63a59
Karl Litterfeldt <Kalle.Litterfeldt@gmail.com>
2013-10-06 11:01:25 +0200

    can now get book objects directly from the database on id and name

--- src/com/Litterfeldt/AStory/dbConnector/dbBook.java
+++ src/com/Litterfeldt/AStory/dbConnector/dbBook.java
@@ -3,6 +3,8 @@ package com.Litterfeldt.AStory.dbConnector;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
+
 import com.Litterfeldt.AStory.models.Book;
 import com.Litterfeldt.AStory.models.Chapter;
 import java.util.ArrayList;
@@ -18,11 +20,20 @@ public class dbBook {
     public static void addBook(Context c, Book b){
         (new dbBook(c)).addBook(b);
     }
+    public static Book bookById(Context c, int id){
+        return (new dbBook(c)).getBookById(id);
+    }
+
+    public static int bookIdByName(Context c, String name){
+        return (new dbBook(c)).getBookId(name);
+    }
+
 
     private static final String ALL_BOOKS = "SELECT * FROM "+ dbConnector.TABLE_NAME_BOOK_LIST + " ;";
-    private static final String ALL_CHAPTERS_BY_BOOK = "SELECT * FROM" + dbConnector.TABLE_NAME_CHAPTER_LIST + " WHERE BOOK_ID = ? ;";
-    private static final String IMAGE = "Select IMG_BLOB from" + dbConnector.TABLE_NAME_BOOK_IMG +  "where BOOK_ID = '?';";
-    private static final String PURGE = "DELETE * FROM ";
+    private static final String ALL_BOOKS_BY_ID = "SELECT * FROM " + dbConnector.TABLE_NAME_BOOK_LIST + " WHERE "+dbConnector.COLUMN_BOOK_LIST_ID+" = ? ;";
+    private static final String ALL_CHAPTERS_BY_BOOK = "SELECT * FROM " + dbConnector.TABLE_NAME_CHAPTER_LIST + " WHERE BOOK_ID = ? ;";
+    private static final String IMAGE = "Select IMG_BLOB from " + dbConnector.TABLE_NAME_BOOK_IMG +  " where BOOK_ID = ?;";
+    private static final String PURGE = "DELETE FROM ";
 
     private dbConnector connector;
 
@@ -50,28 +61,48 @@ public class dbBook {
         initialValues.put(dbConnector.COLUMN_BOOK_LIST_AUTHOR,book.author());
         connector.write().insert(dbConnector.TABLE_NAME_BOOK_LIST, null, initialValues);
         int bookID = getBookId(book.name());
+        Log.e("addBook",Integer.toString(bookID));
+
         for (Chapter chapter : book.getChapters()){
             addChapter(chapter, bookID);
         }
         addImg(bookID,book.image());
     }
     public int getBookId(String bookName){
-        return connector.read().query(dbConnector.TABLE_NAME_BOOK_LIST,
+        Cursor c = connector.read().query(dbConnector.TABLE_NAME_BOOK_LIST,
                 new String[]{dbConnector.COLUMN_BOOK_LIST_ID},
-                dbConnector.COLUMN_BOOK_LIST_NAME+"="+bookName,
-                null,null,null,null).getInt(0);
+                dbConnector.COLUMN_BOOK_LIST_NAME+"="+"\""+bookName+"\"",
+                null,null,null,null);
+        c.moveToFirst();
+        return c.getInt(0);
+    }
+    private Book getBookById(int id){
+        Cursor bookCursor = connector.read().rawQuery(ALL_BOOKS_BY_ID.replace("?",Integer.toString(id)), null);
+        boolean notEmpty = bookCursor.moveToFirst();
+        if (notEmpty) {
+            String name = bookCursor.getString(1);
+            String author = bookCursor.getString(2);
+            ArrayList<Chapter> chapters = getAllChapters(id);
+            byte[] img = getImageById(id);
+            Book book = new Book(id,name, author, chapters, img);
+            return book;
+        } else {
+            return null;
+        }
+
+
     }
     public void purgeAllBooks(){
-        connector.write().execSQL(PURGE + dbConnector.TABLE_NAME_BOOK_LIST );
-        connector.write().execSQL(PURGE + dbConnector.TABLE_NAME_CHAPTER_LIST );
-        connector.write().execSQL(PURGE + dbConnector.TABLE_NAME_BOOK_IMG );
+        connector.write().execSQL(PURGE + dbConnector.TABLE_NAME_BOOK_LIST +";" );
+        connector.write().execSQL(PURGE + dbConnector.TABLE_NAME_CHAPTER_LIST+";" );
+        connector.write().execSQL(PURGE + dbConnector.TABLE_NAME_BOOK_IMG+";" );
     }
     public ArrayList<Book> getAllBooks() {
         Cursor bookCursor = connector.read().rawQuery(ALL_BOOKS, null);
         boolean notEmpty = bookCursor.moveToFirst();
         if (notEmpty) {
             ArrayList<Book> books = new ArrayList<Book>();
-            do {
+            while(!bookCursor.isAfterLast()){
                 int id = bookCursor.getInt(0);
                 String name = bookCursor.getString(1);
                 String author = bookCursor.getString(2);
@@ -79,10 +110,11 @@ public class dbBook {
                 byte[] img = getImageById(id);
                 Book book = new Book(id,name, author, chapters, img);
                 books.add(book);
-            } while (!bookCursor.isLast());
+                bookCursor.moveToNext();
+            }
             return books;
         } else {
-            return null;
+            return new ArrayList<Book>();
         }
     }
     public byte[] getImageById(int bookId) {
@@ -100,7 +132,7 @@ public class dbBook {
         boolean notEmpty = chapterCursor.moveToFirst();
         if (notEmpty) {
             ArrayList<Chapter> chapters = new ArrayList<Chapter>();
-            do {
+            while(!chapterCursor.isAfterLast()){
                 int chapterID = chapterCursor.getInt(0);
                 int chapterNr = chapterCursor.getInt(2);
                 String chapterPath = chapterCursor.getString(3);
@@ -108,8 +140,7 @@ public class dbBook {
                 Chapter chapter = new Chapter(chapterID, chapterPath, chapterNr, chapterDuration);
                 chapters.add(chapter);
                 chapterCursor.moveToNext();
-
-            } while (!chapterCursor.isLast());
+            }
 
             return chapters;
         } else {
