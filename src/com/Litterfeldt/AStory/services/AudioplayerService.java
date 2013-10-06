84404fb1e24f916684f11ea06b19030dcf4dd674
Karl Litterfeldt <Kalle.Litterfeldt@gmail.com>
2013-10-06 03:07:54 +0200

    completly rewrote the AP-service

--- src/com/Litterfeldt/AStory/services/AudioplayerService.java
+++ src/com/Litterfeldt/AStory/services/AudioplayerService.java
@@ -12,32 +12,32 @@ import android.os.IBinder;
 
 import android.util.Log;
 import com.Litterfeldt.AStory.R;
+import com.Litterfeldt.AStory.dbConnector.dbBook;
+import com.Litterfeldt.AStory.models.Book;
 import com.Litterfeldt.AStory.customClasses.CoreApplication;
 import com.Litterfeldt.AStory.customClasses.CustomMediaPlayer;
-import com.Litterfeldt.AStory.customClasses.SqlConnector;
 import com.Litterfeldt.AStory.pagerView;
 
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class AudioplayerService extends Service implements MediaPlayer.OnCompletionListener{
 
     private final IBinder mBinder = new AudioplayerBinder();
 
-    public CustomMediaPlayer mp;
-    public SqlConnector sqlConnector;
-    public ArrayList<ArrayList<String>> booklist;
-    public ArrayList<ArrayList<String>> currentBookChapterList;
+    private CustomMediaPlayer mp;
+
+    private ArrayList<Book> booklist;
 
     @Override
     public void onCreate(){
         super.onCreate();
         mp = new CustomMediaPlayer();
-        sqlConnector = new SqlConnector(this);
         mp.setOnCompletionListener(this);
+        populateBookList();
         showNotification();
     }
+
     //--- SERVICE MANAGEMENT ---
     @Override
     public int onStartCommand(Intent intent, int flags, int startID){
@@ -48,28 +48,13 @@ public class AudioplayerService extends Service implements MediaPlayer.OnComplet
         return START_STICKY;
     }
     public void stopThisServiceNow(){
-
-        if(mp.hasCurrentBook){
-            try{
-                sqlConnector.clearCach();
-                sqlConnector.newSave(sqlConnector.getBookIDFromName(mp.currentBookname),mp.currentChapterIndex,mp.getCurrentPosition());
-                Log.w("SQL/SAVE","Made save!");
-            }
-            catch (Exception e){
-                Log.w("SQL/SAVE","Couldnt make save");
-            }
-        }
         mp.release();
         mp=null;
-        sqlConnector.close();
-        sqlConnector= null;
         booklist = null;
-        currentBookChapterList= null;
         ((CoreApplication)getApplication()).serviceStarted = false;
         this.stopForeground(true);
         this.stopSelf();
         android.os.Process.killProcess(android.os.Process.myPid());
-
     }
     public class AudioplayerBinder extends Binder {
         public AudioplayerService getService(){
@@ -82,40 +67,19 @@ public class AudioplayerService extends Service implements MediaPlayer.OnComplet
     }
 
     //--- SERVICE METHODS ---
-    public void getBookList(){
-        booklist = sqlConnector.getBooklist();
-    }
-    public void getChapters(String Bookname){
-        currentBookChapterList = sqlConnector.getChapters(Bookname);
+    public ArrayList<Book> getBookList(){
+        return dbBook.getBooks(((CoreApplication) getApplication()).getApplicationContext());
     }
 
     //--- HELPER METHODS ---
-    public void playBook(String Bookname,int Chapterindex){
-        try{
-            mp.stop();
-            mp.reset();
-            getChapters(Bookname);
-            mp.datasource = currentBookChapterList.get(Chapterindex).get(2);
-            mp.currentBookname = currentBookChapterList.get(Chapterindex).get(0);
-            mp.currentChapterIndex = Chapterindex;
-            mp.setDataSource(mp.datasource);
-            mp.prepare();
-            mp.start();
-            mp.playerStartedPlayingABook =true;
-            mp.hasCurrentBook = true;
-            mp.isPlaying=true;
-            mp.backgroundIsSet = false;
-            mp.pauseIMGisSet = true;
 
-        } catch (IOException e) {e.printStackTrace();}
-    }
     @SuppressWarnings("deprecation")
     public void showNotification() {
         CharSequence header;
         CharSequence text;
         int drw ;
-        if(mp.isPlaying){
-            text = mp.currentBookname.trim();
+        if(mp.isPlaying()){
+            text = mp.book().name().trim();
             header = "Playing:";
             drw = R.drawable.play;
             Notification notification = new Notification(drw, text,
@@ -140,18 +104,29 @@ public class AudioplayerService extends Service implements MediaPlayer.OnComplet
         }
 
     }
+    public void populateBookList(){
+        booklist =dbBook.getBooks(this.getApplicationContext());
+    }
+    public CustomMediaPlayer getMediaPlayer(){
+        return mp;
+    }
     @Override
     public void onCompletion(MediaPlayer cp) {
-        try{
-        if(mp.currentChapterIndex < currentBookChapterList.size()){
-            playBook(mp.currentBookname, mp.currentChapterIndex + 1);
-            mp.playerStartedPlayingABook = true;
-            mp.hasCurrentBook = true;
-        }
-        else{
-            mp.stop();
-            mp.playerStartedPlayingABook = false;
-            mp.hasCurrentBook =false;
-        }}catch (Exception e){}
+        nextChapter();
     }
+    public void nextChapter(){
+        boolean success = mp.nextChapter();
+        if (!success) {
+            mp.stop();
+            mp.reset();
+            mp.removeBook();
+        }
+    }
+    public void previousChapter(){
+        boolean success = mp.nextChapter();
+        if (!success) {
+            mp.seekTo(0);
+        }
+    }
+
 }
