344eb0a44636a434a24c6d6eab0a3a51c38861d9
Karl Litterfeldt <Kalle.Litterfeldt@gmail.com>
2014-05-18 16:56:18 +0200

    removed unused import

--- src/com/Litterfeldt/AStory/services/AudioplayerService.java
+++ src/com/Litterfeldt/AStory/services/AudioplayerService.java
@@ -16,7 +16,6 @@ import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
 
-import android.provider.ContactsContract;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
