585d80c68e03c121043436f95f84eee3bde06dd3
Karl Litterfeldt <Kalle.Litterfeldt@gmail.com>
2014-05-18 18:27:43 +0200

    removed unnecessary imports.

--- src/com/Litterfeldt/AStory/fragments/LibraryFragment.java
+++ src/com/Litterfeldt/AStory/fragments/LibraryFragment.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
